ab88b7d5b7384849ccf996717728d2b81f9786fc
Apertium <zuzana22@github.com>
2017-09-26 19:04:39 +0200

    tok

--- corpus/tokeniser.py
+++ corpus/tokeniser.py
@@ -2,17 +2,19 @@ import sys
 
 lines = sys.stdin.readlines()
 
-s=1
+e = 1
 
 for line in lines:
-	print(s, line)
-	s = s + 1
-	line=line.split(" ")
+	print("# sent_id =",e)
+	print("# text =", line.strip())
+	line=line.strip().split(" ")
 	index = 1
 	for word in line:
 		word = word.replace(" ", "").replace(".","").replace(":","").replace(")","").replace("?","").replace(",","").replace("(","")
 		print(index, '\t', word, '\t','_', '\t','_','\t','_','\t','_')	
 		index = index + 1
+	e = e+1
+	print()
 	
 
 
